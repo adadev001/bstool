52cd1d433a4b44d08a257d4c4bd143ddb217f015
adadev001 <adadev001@gmail.com>
2026-02-08 06:38:08 +0000

    RSStest

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+import feedparser
+
+RSS_URL = "https://feeds.feedburner.com/TheHackersNews"
+
 def main():
-    print("GitHub Actions test: OK")
+    feed = feedparser.parse(RSS_URL)
+
+    print(f"feed title: {feed.feed.get('title')}")
+
+    print("latest entries:")
+    for entry in feed.entries[:5]:
+        print("-")
+        print("title:", entry.get("title"))
+        print("link:", entry.get("link"))
 
 if __name__ == "__main__":
     main()
