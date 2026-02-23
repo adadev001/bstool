3cae26fdf1759ac58f0c7a5748c6f266a325c4c6
adadev001 <adadev001@gmail.com>
2026-02-23 13:16:05 +0000

    JVNの判定名(type)が異なるのでsites.yamlの内容を含めるように修正

--- main.py
+++ main.py
@@ -385,7 +385,7 @@ def main():
             items = fetch_rss(site, since, until)
         elif site["type"] == "nvd_api":
             items = fetch_nvd(site, since, until)
-        elif site["type"] == "jvn":
+        elif site["type"] in ("jvn", "jvn_rss"):
             items = fetch_jvn(site, since, until)
             logging.info(f"[{site_key}] JVN entries:{len(items)} since={since} until={until}")
         else:
